e3995f79974b8dd7c4d969bab0b5633b07a5b4cf
Beka <beka@bekabug.com>
2019-05-30 19:10:43 -0400

    Find area of a circle given the radius. Pi to 2 decimal places

---
+++ radius.py
@@ -0,0 +1,5 @@
+'''find area of a circle given the radius'''
+RAD = int(input("What's the radius: "))
+AREA = 3.14 * RAD**2
+print(AREA)
+## Holy shit i finished this in 7 minutes but now i need to lint
