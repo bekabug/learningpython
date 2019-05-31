0a9caee676bac3a3590d5582a9e2aad258b32502
Beka <beka@bekabug.com>
2019-05-30 20:17:40 -0400

    reverse a string with slice

---
+++ name_scramble.py
@@ -0,0 +1,4 @@
+'''take input of users name then print it in reverse with a space between each letter'''
+NAME = input('What\'s your name?')
+REVERSE_NAME = NAME[::-1]
+print('In the mirror your name is', REVERSE_NAME)
